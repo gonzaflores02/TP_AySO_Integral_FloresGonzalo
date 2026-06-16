2d1b660eb3fe7d51647c30366d9696fef084f220
Gonzalo Flores <gonzalofranciscoflores@gmail.com>
2026-06-16 00:31:52 +0000

    agrego script bash

--- Bash_script/check_url/check_URL.sh
+++ Bash_script/check_url/check_URL.sh
@@ -1,36 +1,29 @@
 #!/bin/bash
 clear
 
-###############################
-#
-# Parametros:
-#  - Lista Dominios y URL
-#
-#  Tareas:
-#  - Se debera generar la estructura de directorio pedida con 1 solo comando con las tecnicas enseñadas en clases
-#  - Generar los archivos de logs requeridos.
-#
-###############################
 LISTA=$1
-
 LOG_FILE="/var/log/status_url.log"
+
+mkdir -p /tmp/head-check/{Error/{cliente,servidor},ok}
 
 ANT_IFS=$IFS
 IFS=$'\n'
-
-#---- Dentro del bucle ----#
-  # Obtener el código de estado HTTP
+for LINEA in $(cat $LISTA | grep -v ^#)
+do
+  DOMINIO=$(echo $LINEA | awk '{print $1}')
+  URL=$(echo $LINEA | awk '{print $2}')
   STATUS_CODE=$(curl -LI -o /dev/null -w '%{http_code}\n' -s "$URL")
-
-  # Fecha y hora actual en formato yyyymmdd_hhmmss
   TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+  LOGLINE="$TIMESTAMP - Code:$STATUS_CODE - URL:$URL"
 
+  echo "$LOGLINE" | sudo tee -a "$LOG_FILE"
 
- # Registrar en el archivo /var/log/status_url.log
-  echo "$TIMESTAMP - Code:$STATUS_CODE - URL:$URL" |sudo tee -a  "$LOG_FILE"
-
-
-
-#-------------------------#
-
+  if [[ $STATUS_CODE -eq 200 ]]; then
+    echo "$LOGLINE" >> /tmp/head-check/ok/$DOMINIO.log
+  elif [[ $STATUS_CODE -ge 400 && $STATUS_CODE -lt 500 ]]; then
+    echo "$LOGLINE" >> /tmp/head-check/Error/cliente/$DOMINIO.log
+  elif [[ $STATUS_CODE -ge 500 && $STATUS_CODE -lt 600 ]]; then
+    echo "$LOGLINE" >> /tmp/head-check/Error/servidor/$DOMINIO.log
+  fi
+done
 IFS=$ANT_IFS
